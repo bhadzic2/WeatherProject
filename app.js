52dd78e486a3cf39b73cac0e16343b6417139994
Belma Hadžić <bhadzic2@etf.unsa.ba>
2022-08-25 16:55:54 +0200

    post

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname+"/index.html");
-  });
+  res.sendFile(__dirname + "/index.html");
+});
 
+app.post("/", function(req, res) {
+  console.log("Post request received.");
+})
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000.");
